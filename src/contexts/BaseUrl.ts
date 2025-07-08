4c3f73c31a8e3869b2eb30ea17c759c1ae89a97f
Lok <loktseweb@gmail.com>
2025-07-08 16:29:13 +0800

    update API BaseUrl

--- src/contexts/BaseUrl.ts
+++ src/contexts/BaseUrl.ts
@@ -1,6 +1,6 @@
 // 使用環境變數，如果未設置則使用生產環境 URL 作為預設值
 //const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://rent-car-one-rust.vercel.app";
-const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://vercel-express-app-production.up.railway.app";
+const baseUrl = "https://vercel-express-app-production.up.railway.app";
 
 //local
 //const baseUrl = "http://localhost:5000";
